41a693d14bf03d2a3aee7f714d83d8eaae86ee7a
Dondrey Taylor <ddt7@njit.edu>
2015-10-27 01:30:03 -0400

    Drop in replacement for PanGestureRecognizer. Provides direction of pan gesture.

---
+++ PanDirectionGestureRecognizer.swift
@@ -0,0 +1,40 @@
+//
+//  PanDirectionGestureRecognizer.swift
+//  MiniChat
+//
+//  Created by Dondrey Taylor on 8/24/15.
+//  Copyright © 2015 MiniChat, Inc. All rights reserved.
+//
+
+import UIKit
+import UIKit.UIGestureRecognizerSubclass
+
+enum PanDirection {
+    case Vertical
+    case Horizontal
+}
+
+class PanDirectionGestureRecognizer: UIPanGestureRecognizer {
+    
+    let direction : PanDirection
+    
+    init(direction: PanDirection, target: AnyObject, action: Selector) {
+        self.direction = direction
+        super.init(target: target, action: action)
+    }
+
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent) {
+        super.touchesMoved(touches, withEvent: event)
+        if state == .Began {
+            let velocity = velocityInView(self.view!)
+            switch direction {
+            case .Horizontal where fabs(velocity.y) > fabs(velocity.x):
+                state = .Cancelled
+            case .Vertical where fabs(velocity.x) > fabs(velocity.y):
+                state = .Cancelled
+            default:
+                break
+            }
+        }
+    }
+}
